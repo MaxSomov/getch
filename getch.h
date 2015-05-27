d60ef5df32a48996cec117aa9e44985abd717e15
MaxSomov <sirbormotron@gmail.com>
2015-05-27 15:55:23 +0300

    1

---
+++ getch.h
@@ -0,0 +1,19 @@
+#ifndef GETCH_H
+#define GETCH_H
+
+#include <termios.h>
+
+int getch( ) {
+struct termios oldt,
+newt;
+int ch;
+tcgetattr( STDIN_FILENO, &oldt );
+newt = oldt;
+newt.c_lflag &= ~( ICANON | ECHO );
+tcsetattr( STDIN_FILENO, TCSANOW, &newt );
+ch = getchar();
+tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
+return ch;
+}
+
+#endif // GETCH_H
